0e5137e3a8150b9dc27535b749f75f589af00485
Sarah El-Guindy <sarah.elguindy@kellogg.northwestern.edu>
2025-01-27 05:07:05 +0000

    completed code up till data inputs

--- kmdb.sql
+++ kmdb.sql
@@ -112,13 +112,74 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 -- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS characters;
 
 -- Create new tables, according to your domain model
 -- TODO!
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_name TEXT,
+    release_date INTEGER,
+    MPAA_rating TEXT,
+    studio_id INTEGER
+);
+
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    studio_name TEXT
+);
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    actor_name TEXT
+);
+
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character_name TEXT,
+    actor_id INTEGER,
+    movie_id INTEGER
+);
+
+
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 -- TODO!
+
+INSERT INTO studios (
+    studio_name
+) VALUES (
+    "Warner Bros."
+);
+
+INSERT INTO movies (
+    movie_name,
+    release_date,
+    MPAA_rating,
+    studio_id
+) VALUES 
+    ("Batman Begins", "2005", "PG-13", 1), 
+    ("The Dark Knight", "2008", "PG-13", 1),
+    ("The Dark Knight Rises", "2012", "PG-13", 1);
+
+INSERT INTO actors (
+    actor_name
+) VALUES 
+    ("Christian Bale"), 
+    ("Michael Caine"), 
+    ("Liam Neeson"), 
+    ("Katie Holmes"), 
+    ("Gary Oldman"), 
+    ("Heath Ledger"), 
+    ("Aaron Eckhart"),
+    ("Maggie Gyllenhaal"), 
+    ("Tom Hardy"), 
+    ("Joseph Gordon-Levitt"), 
+    ("Anne Hathaway");
 
 -- Prints a header for the movies output
 .print "Movies"
